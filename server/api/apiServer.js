aa22e192b685a3b1df3b0b582d7600d7f1bb3b20
lllidan120 <lllidan120@163.com>
2018-06-27 08:23:39 +0800

    first commit

---
+++ server/api/apiServer.js
@@ -0,0 +1,61 @@
+/**
+ * api请求server
+ *
+ * 0：成功
+ * 1：数据不合法
+ * 2：客户端数据错误
+ * 3：后端错误
+ */
+import Express from 'express'
+import config from '../../config/config'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+
+const port = config.apiPort;
+
+const app = new Express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser('express_react_cookie'));
+app.use(session({
+    secret:'express_react_cookie',
+    resave: true,
+    saveUninitialized:true,
+    cookie: {maxAge: 60 * 1000 * 30}//过期时间
+}));
+
+
+//展示页面路由
+app.use('/', require('./main'));
+//管理页面路由
+app.use('/admin', require('./admin'));
+
+// const options = {
+//     useMongoClient: true,
+//     user: 'aming',
+//     pass: 'aming',
+//     autoIndex: false, // Don't build indexes
+//     reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+//     reconnectInterval: 500, // Reconnect every 500ms
+//     poolSize: 10, // Maintain up to 10 socket connections
+//     // If not connected, return errors immediately rather than waiting for reconnect
+//     bufferMaxEntries: 0
+// };
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(`mongodb://${config.dbHost}:${config.dbPort}/blog`, function (err) {
+    if (err) {
+        console.log(err, "数据库连接失败");
+        return;
+    }
+    console.log('数据库连接成功');
+
+    app.listen(port, function (err) {
+        if (err) {
+            console.error('err:', err);
+        } else {
+            console.info(`===> api server is running at ${config.apiHost}:${config.apiPort}`)
+        }
+    });
+});
